906bef3be1e136804f8dd55a78a6e630423dabea
mhagerty40 <60339851+mhagerty40@users.noreply.github.com>
2020-03-02 20:55:37 -0800

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ port = 8080;
 var file = new static.Server(directory);
 
 /*Construct an http server that gets files from the file server*/
-var app =http.createServer(
+var app = http.createServer(
   function(request,response) {
     request.addListener('end',
       function(){
