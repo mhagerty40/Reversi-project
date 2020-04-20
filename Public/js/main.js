a4108fa7ea2271be9ea7e7e8bdd2107ba6141e18
michaelhag <mhagerty@westmont.edu>
2020-04-20 14:25:14 -0700

    here we go

--- Public/js/main.js
+++ Public/js/main.js
@@ -343,6 +343,7 @@ var old_board = [
                 ];
 
   var my_color = ' ';
+  var interval_timer;
 
 socket.on('game_update', function(payload)
 {
@@ -380,8 +381,29 @@ else
   return;
 }
 $('#my_color').html('<h3 id="my_color">Team '+my_color+'</h3>');
+$('#my_color').append('<h4>It is '+payload.game.whose_turn+'\'s turn. Elasped time <span id="elapsed"></h4>');
 
+clearInterval(interval_timer);
+interval_timer = setInterval( function(last_time)
+{
+  return function()
+  {
+  //Do the work of updating the UI//
+  var d = new Date();
+  var elapsedmilli = d.getTime() - last_time;
+  var minutes = Math.floor(elapsedmilli / (60 * 1000));
+  var seconds = Math.floor((elapsedmilli % (60 * 1000))/ 1000);
 
+  if(seconds < 10)
+  {
+    $('#elapsed').html(minutes+'0'+seconds);
+  }
+  else
+  {
+    $('#elapsed').html(minutes+':'+seconds);
+  }
+  }}(payload.game.last_move_time)
+      , 1000);
 
 /* Animate changes to the board*/
 
@@ -442,12 +464,18 @@ for(row = 0; row < 8; row++)
         else {
                 $('#'+row+'_'+column).html('<img src="assets/images/error.gif" alt="error"/>');
              }
-             /* set up  interactivity*/
-             $('#'+row+'_'+column).off('click');
-             if(board[row][column]== ' ')
-             {
-               $('#'+row+'_'+column).addClass('hovered_over');
-               $('#'+row+'_'+column).click(function(r,c)
+          }
+
+    /* set up  interactivity*/
+    $('#'+row+'_'+column).off('click');
+    $('#'+row+'_'+column).removeClass('hovered_over');
+
+    if(payload.game.whose_turn === my_color)
+    {
+      if(payload.game.legal_moves[row][column] === my_color.substring(0,1))
+      {
+           $('#'+row+'_'+column).addClass('hovered_over');
+           $('#'+row+'_'+column).click(function(r,c)
                {
                  return function()
                  {
@@ -459,11 +487,8 @@ for(row = 0; row < 8; row++)
                    socket.emit('play_token', payload);
                  };
                }(row,column));
-             }
-             else
-             {
-               $('#'+row+'_'+column).removeClass('hovered_over');
-             }
+        }
+
       }
     }
   }
